eb20756399ea312430c6b0392b08b924598d2f5c
komalmastud7970@gmail.com <komalmastud7970@gmail.com>
2024-11-25 14:20:11 +0530

    understanding versoningin Node Js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.send("Hello from home page!");
+  return res.send("Hello from Home page!");
 });
 app.get("/about", (req, res) => {
   return res.send(`Hello ${req.query.name}`);
