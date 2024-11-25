e4325b5af57e5f6ca692ced9643396b080132fec
komalmastud7970@gmail.com <komalmastud7970@gmail.com>
2024-11-25 13:32:17 +0530

    expressJs working

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url"); // Corrected the module
+// Correct module inclusion
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-  if (req.url === "/favicon.ico") return res.end();
+const app = express();
 
-  // Log the request
-  const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
-  fs.appendFile("log.txt", log, (err) => {
-    if (err) {
-      console.error("Error writing to log file:", err);
-    }
-  });
-
-  // Parse the URL
-  const myUrl = url.parse(req.url, true);
-  // Corrected the logging
-
-  // Handle routes
-  switch (myUrl.pathname) {
-    case "/":
-      if (req.method === "GET") res.end("Welcome to the Home Page!");
-      break;
-
-    case "/about":
-      const username = myUrl.query.myname || "Guest";
-      res.end(`Hi, ${username}`);
-      break;
-
-    case "/search":
-      const search = myUrl.query.search_query || "nothing";
-      res.end("Here are your results for " + search);
-      break;
-
-    case "/signup":
-      if (req.method === "GET") res.end("This is a signup form");
-      else if (req.method === "POST") {
-        res.end("Sucess");
-      }
-    default:
-      res.statusCode = 404;
-      res.end("404 Not Found");
-      break;
-  }
+app.get("/", (req, res) => {
+  return res.send("Hello from home page!");
+});
+app.get("/about", (req, res) => {
+  return res.send(`Hello ${req.query.name}`);
 });
 
-// Start the server
-myServer.listen(8000, () => console.log("Server Started!"));
+// Handler function
+
+// Create the server
+app.listen(8000, () => console.log("server started!"));
